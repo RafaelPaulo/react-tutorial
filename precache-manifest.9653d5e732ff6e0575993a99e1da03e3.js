80525afdb7c0743ce9755bda937c010fc8f3041f
RafaelPaulo <rafael-sq@hotmail.com>
2018-12-11 19:11:01 -0200

    Updates

---
+++ precache-manifest.9653d5e732ff6e0575993a99e1da03e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "a092159437e94a2cb683",
+    "url": "/react-tutorial/static/js/main.a0921594.chunk.js"
+  },
+  {
+    "revision": "2818dbc80ba3c939eefa",
+    "url": "/react-tutorial/static/js/1.2818dbc8.chunk.js"
+  },
+  {
+    "revision": "a092159437e94a2cb683",
+    "url": "/react-tutorial/static/css/main.c2ea7c98.chunk.css"
+  },
+  {
+    "revision": "f9ece12e0a5b1f22374a17a9cb3bca85",
+    "url": "/react-tutorial/index.html"
+  }
+];
